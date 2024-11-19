cd4e09be3adcba99560f8f6320383b00403dc727
KoodiApina <sulonkoodit@gmail.com>
2024-11-20 00:04:36 +0200

    Validation complete?

--- Lab2/src/main.c
+++ Lab2/src/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "../include/numbers.h"
 
 #define STRTOL_BASE 10
 
 int main(int argc, char *argv[]) 
 {
-	char *endptr;
-	long val;
-	
 	int min, max, uniqc;
 
 	if (argc < 4)
@@ -20,7 +18,13 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-   				
+		for (int i = 1; i < argc; i++)
+		{
+			if (isdigit(argv[i])) 
+			{
+				printf("Valid integer found!\n");
+			}
+		}
 	}	
 	generate_numbers(min, max, uniqc);
 	return 0;
