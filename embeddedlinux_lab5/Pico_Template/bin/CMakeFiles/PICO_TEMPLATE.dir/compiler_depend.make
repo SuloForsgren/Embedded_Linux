382340fa954f15b49430200cdcf3bf673ba469e1
SuloForsgren <sulo.forsgren@gmail.com>
2024-11-25 17:41:43 +0200

    Blinky working :3

---
+++ embeddedlinux_lab5/Pico_Template/bin/CMakeFiles/PICO_TEMPLATE.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PICO_TEMPLATE.
+# This may be replaced when dependencies are built.
---
+++ embeddedlinux_lab5/Pico_Template/bin/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/sulo/dev/programming/iot/School/Embedded_Linux/Lab5/Pico_Template/bin/_deps/picotool/"
+};
